c7148c1f4c218ac689d2a151b259fad44ddf56ad
AsheLight <nagornov-1995@inbox.ru>
2023-07-16 15:17:01 +0300

    HW 100%

--- HW6.cs
+++ HW6.cs
@@ -1,3 +1,42 @@
 ﻿//Task 41
+static void Main(string[] args)
+    {
+        Console.Write("Введите числа через запятую: ");
+        string input = Console.ReadLine();
 
+        string[] numbers = input.Split(',');
+
+        int count = 0;
+        foreach (string number in numbers)
+        {
+            int num = int.Parse(number.Trim());
+            if (num > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Количество чисел, больших нуля: " + count);
+    }
 //Task 43
+static void Main (string[] args)
+{
+        Console.WriteLine("Введите значение b1:");
+        double b1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение k1:");
+        double k1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение b2:");
+        double b2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение k2:");
+        double k2 = double.Parse(Console.ReadLine());
+
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+
+        Console.WriteLine("Точка пересечения двух прямых: ({0}, {1})", x, y);
+
+        Console.ReadLine();
+    }
